4812794d3942e49f5cf5d4bfa4e130de5491f388
pakidev(Milos Pavlovic) <milospavlovicdev@gmail.com>
2025-06-03 17:37:13 +0200

    Initial commit

---
+++ PostsApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PostsApp
+//
+//  Created by Milos Pavlovic on 3. 6. 2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PostsApp/PostsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsAppApp.swift
+//  PostsApp
+//
+//  Created by Milos Pavlovic on 3. 6. 2025..
+//
+
+import SwiftUI
+
+@main
+struct PostsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
